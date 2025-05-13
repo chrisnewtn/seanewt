54b88d24433951416f3d2ab8a9048bc50925663e
Chris Newton <me@chrisnewtn.com>
2025-05-13 11:20:08 +0100

    chore: de-duplicate assets and make 100% of `public` generated
    The structure of the public directory is left over from when I was
    considering committing the whole public directory with every commit,
    before deciding this was impractical.
    
    Having just one directory where web assets are stored, while it's
    confusing that some are used to generate pages then discarded, and some
    are now copied; I think it's simpler to understand at a glance.

--- doc-processor/cli.js
+++ doc-processor/cli.js
@@ -1,3 +1,4 @@
+import {minimatch} from 'minimatch';
 import fs from 'node:fs/promises';
 import {createReadStream, createWriteStream} from 'node:fs';
 import {pipeline} from 'node:stream/promises';
@@ -42,6 +43,88 @@ const fileCache = new FileCache();
 const assets = new Map();
 const writtenAssets = new Set();
 
+/**
+ * @param {string} pathToDir
+ * @param {Dirent} inputFile
+ */
+async function processFile(pathToDir, inputFile) {
+  const ext = path.extname(inputFile.name);
+
+  const pathToInput = path.join(pathToDir, inputFile.name);
+  const pathToOutputDir = path.join(outputDir, path.relative(inputDir, pathToDir));
+
+  console.log('processing', pathToInput);
+  const fileContents = await fileCache.get(pathToInput);
+
+  const vFile = await processDocument({
+    inputFile: {
+      name: pathToInput,
+      text: fileContents
+    },
+    assets,
+    skipImageOptimization,
+    fileCache,
+    writtenAssets,
+    outputDir: pathToOutputDir
+  });
+
+  await ensureDirectory(pathToOutputDir);
+
+  for (const [pathToOldAsset, pathToNewAsset] of assets) {
+    if (writtenAssets.has(pathToNewAsset)) {
+      continue;
+    }
+
+    await ensureDirectory(path.dirname(pathToNewAsset));
+
+    console.log('write', pathToNewAsset);
+
+    await pipeline(
+      createReadStream(pathToOldAsset),
+      createWriteStream(pathToNewAsset)
+    );
+
+    writtenAssets.add(pathToNewAsset);
+  }
+
+  const outputName = `${path.basename(inputFile.name, ext)}.html`;
+  const pathToOutput = path.join(pathToOutputDir, outputName);
+
+  console.log('write', pathToOutput);
+  await fs.writeFile(pathToOutput, String(vFile), 'utf8');
+}
+
+const toCopy = [
+  '**/*.ttf',
+  'static/**/*.js',
+  'CNAME',
+  'favicon.io',
+];
+
+/**
+ * @param {string} pathToDir
+ * @param {Dirent} inputFile
+ */
+async function copyFile(pathToDir, inputFile) {
+  const pathToInput = path.join(pathToDir, inputFile.name);
+  const relativeToInput = path.relative(inputDir, pathToInput);
+
+  const copyable = toCopy.some(glob => minimatch(relativeToInput, glob));
+
+  if (!copyable) {
+    return;
+  }
+
+  const pathToOutputDir = path.join(outputDir, path.relative(inputDir, pathToDir));
+
+  await ensureDirectory(pathToOutputDir);
+
+  const pathToOutput = path.join(pathToOutputDir, inputFile.name);
+
+  console.log('copy', pathToOutput);
+  await fs.copyFile(pathToInput, pathToOutput);
+}
+
 async function processDirectory(pathToDir) {
   for (const inputFile of await fs.readdir(pathToDir, {withFileTypes: true})) {
     if (inputFile.isDirectory()) {
@@ -51,52 +134,11 @@ async function processDirectory(pathToDir) {
 
     const ext = path.extname(inputFile.name);
 
-    if (!processable.has(ext) || isTemplate(inputFile.name)) {
-      continue;
+    if (processable.has(ext) && !isTemplate(inputFile.name)) {
+      await processFile(pathToDir, inputFile);
+    } else {
+      await copyFile(pathToDir, inputFile);
     }
-
-    const pathToInput = path.join(pathToDir, inputFile.name);
-    const pathToOutputDir = path.join(outputDir, path.relative(inputDir, pathToDir));
-
-    console.log('processing', pathToInput);
-    const fileContents = await fileCache.get(pathToInput);
-
-    const vFile = await processDocument({
-      inputFile: {
-        name: pathToInput,
-        text: fileContents
-      },
-      assets,
-      skipImageOptimization,
-      fileCache,
-      writtenAssets,
-      outputDir: pathToOutputDir
-    });
-
-    await ensureDirectory(pathToOutputDir);
-
-    for (const [pathToOldAsset, pathToNewAsset] of assets) {
-      if (writtenAssets.has(pathToNewAsset)) {
-        continue;
-      }
-
-      await ensureDirectory(path.dirname(pathToNewAsset));
-
-      console.log('write', pathToNewAsset);
-
-      await pipeline(
-        createReadStream(pathToOldAsset),
-        createWriteStream(pathToNewAsset)
-      );
-
-      writtenAssets.add(pathToNewAsset);
-    }
-
-    const outputName = `${path.basename(inputFile.name, ext)}.html`;
-    const pathToOutput = path.join(pathToOutputDir, outputName);
-
-    console.log('write', pathToOutput);
-    await fs.writeFile(pathToOutput, String(vFile), 'utf8');
   }
 }
 
