3639edac5577248125a9efc65aea59b6e548b25b
Chris Newton <me@chrisnewtn.com>
2026-06-11 14:45:18 +0100

    test: Add file hashing tests

--- test/end-to-end.test.js
+++ test/end-to-end.test.js
@@ -21,12 +21,18 @@ const filesToTest = [
 
 const exec = promisify(childProcess.exec);
 
-/**
- * @param {string} pathToFile
- */
+/** @type Map<string, import("hast").Root> */
+const fileCache = new Map();
+
+/** @param {string} pathToFile */
 async function parseDoc(pathToFile) {
+  if (fileCache.has(pathToFile)) {
+    return fileCache.get(pathToFile);
+  }
   const text = await readFile(pathToFile, 'utf8');
-  return unified().use(rehypeParse).parse(text);
+  const tree = unified().use(rehypeParse).parse(text);
+  fileCache.set(pathToFile, tree);
+  return tree;
 }
 
 describe('end-to-end', () => {
@@ -67,6 +73,42 @@ describe('end-to-end', () => {
       assert.ok(node);
       assert.equal(node.properties.href, 'https://github.com/chrisnewtn/chrisnewtn.github.io/commit/c563c43109733cd71e65ac2b0735a88a6e0a89e3');
       assert.equal(toText(node), 'c563c43');
+    });
+
+    it('links to the hashed stylesheet', async () => {
+      const tree = await parseDoc(file);
+      const node = select('[rel=stylesheet]', tree);
+
+      assert.ok(node);
+      assert.ok(typeof node.properties.href === 'string');
+      assert.match(node.properties.href, /style-feaf30c.css$/);
+    });
+
+    it('uses a hashed svg as the page icon', async () => {
+      const tree = await parseDoc(file);
+      const node = select('[rel=icon]', tree);
+
+      assert.ok(node);
+      assert.ok(typeof node.properties.href === 'string');
+      assert.match(node.properties.href, /newt-6b756e5.svg$/);
+    });
+
+    it('updates img[src] to refer to hashed asset', async () => {
+      const tree = await parseDoc(file);
+      const node = select('a[href*="github.com"] > picture > img', tree);
+
+      assert.ok(node);
+      assert.ok(typeof node.properties.src === 'string');
+      assert.match(node.properties.src, /github-mark-7a0dd11.svg$/);
+    });
+
+    it('updates source[srcset] to refer to hashed asset', async () => {
+      const tree = await parseDoc(file);
+      const node = select('a[href*="github.com"] > picture > source', tree);
+
+      assert.ok(node);
+      assert.ok(typeof node.properties.srcSet === 'string');
+      assert.match(node.properties.srcSet, /github-mark-white-fab00c2.svg$/);
     });
   }));
 });
